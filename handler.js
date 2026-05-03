d1ef55b2ef60b0e852643b2bbe3d6a9a645c013c
mojitopro <mojitopro@gmail.com>
2026-05-03 13:18:37 +0000

    Remove chat banned check for instant response

--- handler.js
+++ handler.js
@@ -1312,7 +1312,6 @@ m.plugin = name
 if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
 let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
-if (!['owner-unbanchat.js'].includes(name) && chat && chat.isBanned && !isROwner) return
 if (
 name != 'owner-unbanchat.js' &&
 name != 'owner-exec.js' &&
